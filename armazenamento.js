d12f6fa89ebbc512ff8bd21a63e1231932b174e2
HENRY DAVI DE LIMA ALVES <henry.alves5@portalsesisp.org.br>
2026-03-11 10:18:13 -0300

    Commit inicial: Estrutura do projeto

---
+++ armazenamento.js
@@ -0,0 +1,5 @@
+let nome = "carlos";
+let idade = 25;
+
+console.log("Nome: "+ nome);
+console.log("Idade: "+ idade);
---
+++ escopo.js
@@ -0,0 +1,10 @@
+function exemplo() {
+    let numero = 10;
+
+    if (numero){
+    let mensagem = "o numero é maior que 5";
+    console.log(mensagem); // Acessivel dentro do bloco if
+    }
+    console.log(numero);
+}
+exemplo();
